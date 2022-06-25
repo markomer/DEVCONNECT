d28e7d30a40a6f4b345972fa31d3d585d7c3988b
Mark Omer <markomerstudio@gmail.com>
2022-06-25 11:05:12 -0600

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+# from django.shortcuts import render
+# line above, auto populated - needed ??
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+  template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+  template_name = 'pages/about.html'
+
+# Create your views here.
