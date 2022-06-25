4d10184d00882201113fb354d6dcf45928ed5897
Mark Omer <markomerstudio@gmail.com>
2022-06-25 12:58:32 -0600

    End of Class 25 June

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('pages/', include('pages.urls')),
+    #path('pages/', include('pages.urls')),
 
 ]
