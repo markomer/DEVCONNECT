fd6af75e6849e9303ed552fa75f69f93cfe75d9b
TreySchneider95 <tschneid95@gmail.com>
2022-07-06 20:34:36 -0600

    added likes and comments

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from .views import (
   PostDetailView, PostUpdateView,
   PostDeleteView,
 )
+from . import views
 
 urlpatterns = [
   path('', PostListView.as_view(), name='post_list'),
@@ -11,4 +12,6 @@ urlpatterns = [
   path('new/', PostCreateView.as_view(), name='post_new'),
   path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
   path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+  path('create-like/<pk>', views.create_like_view, name = 'create_like'),
+  path('create-comment/<pk>', views.create_comment_view, name = 'create_comment'),
 ]
